127cbb6b9f3251a4d754368c0a504df286792720
ilian1902 <ilian.bratanov82@gmail.com>
2016-12-05 01:47:46 +0200

    and try.

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ const http = require("http").Server(app);
 
 //require("./server/routers")(app, config, userValidator);
 
-app.listen(process.env.PORT || 3001, function() {
+app.listen(config.port, function() {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
