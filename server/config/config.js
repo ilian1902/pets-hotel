8f8e197906e294d7199936dd1d6c5a119913f146
ilian1902 <ilian.bratanov82@gmail.com>
2016-12-06 22:07:47 +0200

    Last try. for cloud

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ let rootPath = path.normalize(__dirname + '/../../');
 
 const connectionStrings = {
     production: 'mongodb://ilian82:123456@ds113958.mlab.com:13958/proba-db',
-    development: 'mongodb://localhost:27017/petshotel'
+    development: 'mongodb://ilian82:123456@ds113958.mlab.com:13958/proba-db' //'mongodb://localhost:27017/petshotel'
 };
 
 module.exports = {
