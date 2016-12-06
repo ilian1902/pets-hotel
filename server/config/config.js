94b4047e7f5ddba7b1b0a6a35dd3919efb45628d
ilian1902 <ilian.bratanov82@gmail.com>
2016-12-06 22:11:46 +0200

    Try cloud

--- server/config/config.js
+++ server/config/config.js
@@ -8,15 +8,13 @@ const connectionStrings = {
 
 module.exports = {
     development: {
-        environment: process.env.NODE_ENV || 'development',
         rootPath: rootPath,
-        db: connectionStrings[process.env.NODE_ENV || 'development'],
+        db: connectionStrings.development,
         port: process.env.PORT || 3001
     },
     production: {
-        environment: process.env.NODE_ENV || 'development',
         rootPath: rootPath,
-        db: connectionStrings[process.env.NODE_ENV || 'development'],
+        db: connectionStrings.production,
         port: process.env.PORT || 3001
     }
 };
